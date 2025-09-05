6dc51ba2e8639c91c3ddc5c5cbd9eb67a09f9793
aparnacr046-code <aparnacr046@gmail.com>
2025-09-05 08:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+/*(function sendMail(){
+    var params={
+        name:document.getElementById("Name").value , 
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value
+    };
+ const serviceID="service_x6rjzra";
+ const templateID="template_h3s5hkl";
+
+ emailjs.send(serviceID,templateID,params)
+ .then(
+    res=>{
+        document.getElementById("Name").value= "";
+        document.getElementById("email").value= "";
+        document.getElementById("message").value= "";
+        console.log(res);
+        alert("your message send succesfully")
+    }
+ )
+ .catch(err=>console.log(err));
+})*/
+(function () {
+    emailjs.init("_r7Ic9B7YGyhOJkMQ"); 
+  })();
+
+  function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_x6rjzra";   
+
+    const templateID = "template_h3s5hkl"; 
+
+    emailjs.send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("✅ Your message was sent successfully!");
+      })
+      .catch((err) => console.error("❌ Error:", err));
+  }
+
+  
+  document.getElementById("contactform").addEventListener("submit", function (e) {
+    e.preventDefault();
+    sendMail();
+  });
